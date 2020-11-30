c975dd35572246fe811739a7159d2d4b58fbde56
vlaportetalamon <vlaportetalamon@D20-33.MDNPEDAGO.local>
2020-11-30 14:11:13 +0100

    Revert "Mise en place de la sélection de l'employé avant la suppression ou modification."
    This reverts commit efc41d20e409f0fbaa8a3d3f9cf0f8ece2fe18ee.

--- Personnel/src/commandLine/LigueConsole.java
+++ Personnel/src/commandLine/LigueConsole.java
@@ -22,7 +22,6 @@ public class LigueConsole
 		this.employeConsole = employeConsole;
 	}
 
-
 	Menu menuLigues()
 	{
 		Menu menu = new Menu("Gerer les ligues", "l");
@@ -109,39 +108,23 @@ public class LigueConsole
 		);
 	}
 	
-	private List<Employe> selectionnerEmploye(Ligue ligue)
-	{
-		return new List<>("Sélectionner un employe","s", 
-				() -> new ArrayList<>(ligue.getEmployes()),
-				(element)-> modifierEmploye(element)
-				);
-	}	
-	
 	private Menu gererEmployes(Ligue ligue)
 	{
 		Menu menu = new Menu("Gerer les employes de " + ligue.getNom(), "e");
 		menu.add(afficherEmployes(ligue));
 		menu.add(ajouterEmploye(ligue));
-		//menu.add(supprimerEmploye(ligue));
-		//menu.add(modifierEmploye(ligue));
-		menu.add(selectionnerEmploye(ligue));
+		menu.add(modifierEmploye(ligue));
+		menu.add(supprimerEmploye(ligue));
 		menu.addBack("q");
 		return menu;
 	}
 
-/*	
- 	private List<Employe> supprimerEmploye(final Ligue ligue)
+	private List<Employe> supprimerEmploye(final Ligue ligue)
 	{
 		return new List<>("Supprimer un employe", "s", 
 				() -> new ArrayList<>(ligue.getEmployes()),
 				(index, element) -> {element.remove();}
 				);
-	}
-*/
-	private Option supprimerEmploye(final Employe employe)
-	{
-		return new Option("Supprimer " + employe.getNom() +" "+ employe.getPrenom() ,"s", 
-				() -> {employe.remove();});
 	}
 	
 	private List<Employe> changerAdministrateur(final Ligue ligue)
@@ -152,24 +135,12 @@ public class LigueConsole
 				);
 	}	
 
-
-/*private List<Employe> modifierEmploye(final Ligue ligue)
+	private List<Employe> modifierEmploye(final Ligue ligue)
 	{
 		return new List<>("Modifier un employe", "e", 
 				() -> new ArrayList<>(ligue.getEmployes()),
 				employeConsole.editerEmploye()
 				);
-	}
-*/	
-	
-	private Menu modifierEmploye(Employe employe)
-	{
-		Menu menu = new Menu("Editer l'employe " + employe.getNom() +" "+employe.getPrenom());
-		menu.add(employeConsole.editerEmploye(employe));
-		menu.add(supprimerEmploye(employe));
-		menu.addBack("q");
-		//menu.setAutoBack(2);
-		return menu;	
 	}
 	
 	private Option supprimer(Ligue ligue)
